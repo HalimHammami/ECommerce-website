f4b4c8ca177afa09d8e64584e4a7fa892874aa04
hammami halim <halim.hammami249@gmail.com>
2022-11-17 23:19:39 +0100

    create node js server

--- README.md
+++ README.md
@@ -11,3 +11,13 @@
    1. npm i react-router-dom
    2. create route for home screen
    3. create router for product screen
+7. create nodejs server
+   1. run npm init in back folder
+   2. update package.json set type : module
+   3. add .js to folder 4. npm install express
+   4. create server.js to imports 6. require express
+   5. create route for return back is ready
+   6. move data .js from front to back
+   7. create route for api/products
+   8. return products
+   9. run npm start
---
+++ back/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Nike shirt',
+      slug: 'nike-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', //679*829
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.4,
+      numReviews: 10,
+      description: 'thicc shirt',
+    },
+    {
+      name: 'Nike pant',
+      slug: 'nike-pant',
+      category: 'Pants',
+      image: '/images/p2.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.4,
+      numReviews: 10,
+      description: 'thicc pants',
+    },
+    {
+      name: 'Adidas shirt',
+      slug: 'adidas-shirt',
+      category: 'Shirts',
+      image: '/images/p3.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Adidas',
+      rating: 4.4,
+      numReviews: 10,
+      description: 'thicc shirts',
+    },
+    {
+      name: 'Adidas pant',
+      slug: 'adidas-pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Adidas',
+      rating: 4.4,
+      numReviews: 10,
+      description: 'thicc pants',
+    },
+  ],
+};
+export default data;
